4c8e17b3e3f59ffb2f48b5efd17ee566120ee3be
manjeet <manjeetkumarjnv968@gmail.com>
2021-07-26 09:38:53 +0530

    updated

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('upload here');
+// console.log('upload here');
+burger = document.querySelector('.burger');
+navbar = document.querySelector('.navbar');
+navlist = document.querySelector('.nav-list');
+rightnav = document.querySelector('.rightnav');
+search = document.querySelector('.search');
+
+burger.addEventListener('click', ()=>{
+    navbar.classList.toggle('h-nav-resp');
+    rightnav.classList.toggle('visibility-resp');
+    navlist.classList.toggle('visibility-resp');
+    // search.classList.toggle('visibility-resp');
+})
